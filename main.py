dc63c856f7d314d0682ffa3a60aab08412beafb5
cilindr3161 <118201473+cilindr3161@users.noreply.github.com>
2023-01-12 20:40:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-
+class MainWin(QWidget):
+   def __init__(self):
+       ''' окно, в котором располагается приветствие '''
+       super().__init__()
+ 
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+       self.set_appear()
+ 
+       # создаём и настраиваем графические элементы:
+       self.initUI()
+ 
+       #устанавливает связи между элементами
+       self.connects()
+ 
+       # старт:
+       self.show()
+ 
+   def initUI(self):
+       ''' создаёт графические элементы '''
+       self.btn_next = QPushButton(txt_next)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+ 
+       self.layout = QVBoxLayout()
+       self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout)
+ 
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+ 
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click)
+ 
+   ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+ 
+def main():
+   app = QApplication([])
+   mw = MainWin()
+   app.exec_()
+ 
+main()
